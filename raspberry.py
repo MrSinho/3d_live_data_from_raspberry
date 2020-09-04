267221ba7a24dd4c0db2ad3cae1fcd9892222065
Anas Boustani <68569053+MrSinho@users.noreply.github.com>
2020-09-05 01:12:30 +0200

    Update raspberry.py

--- raspberry.py
+++ raspberry.py
@@ -1,20 +1,12 @@
-# Simple demo of of the ADXL345 accelerometer library.  Will print the X, Y, Z
-# axis acceleration values every half second.
-# Author: Tony DiCola
-# License: Public Domain
 import time
 from datetime import datetime
 import socket
-
-# Import the ADXL345 module.
 import Adafruit_ADXL345
 
-
-# Create an ADXL345 instance.
 accel = Adafruit_ADXL345.ADXL345()
 
 s = socket.socket()
-host = "192.168.1.237"
+host = "192.168.1.237" # <-- Edit this
 print(host)
 port = 8080
 
@@ -24,49 +16,7 @@ s.listen(1)
 conn, addr = s.accept()
 print("connected to " + str(addr))
 
-# Alternatively you can specify the device address and I2C bus with parameters:
-#accel = Adafruit_ADXL345.ADXL345(address=0x54, busnum=2)
-
-# You can optionally change the range to one of:
-#  - ADXL345_RANGE_2_G   = +/-2G (default)
-#  - ADXL345_RANGE_4_G   = +/-4G
-#  - ADXL345_RANGE_8_G   = +/-8G
-#  - ADXL345_RANGE_16_G  = +/-16G
-# For example to set to +/- 16G:
-#accel.set_range(Adafruit_ADXL345.ADXL345_RANGE_16_G)
-
-# Or change the data rate to one of:
-#  - ADXL345_DATARATE_0_10_HZ = 0.1 hz
-#  - ADXL345_DATARATE_0_20_HZ = 0.2 hz
-#  - ADXL345_DATARATE_0_39_HZ = 0.39 hz
-#  - ADXL345_DATARATE_0_78_HZ = 0.78 hz
-#  - ADXL345_DATARATE_1_56_HZ = 1.56 hz
-# Simple demo of of the ADXL345 accelerometer library.  Will print the X, Y, Z
-# axis acceleration values every half second.
-# Author: Tony DiCola
-# License: Public Domain
-import time
-from datetime import datetime
-import socket
-
-# Import the ADXL345 module.
-import Adafruit_ADXL345
-
-
-# Create an ADXL345 instance.
-accel = Adafruit_ADXL345.ADXL345()
-
-s = socket.socket()
-host = "192.168.1.237"
-print(host)
-port = 8080
-
-print("waiting for incoming connections")
-s.bind((host, port))
-s.listen(1)
-conn, addr = s.accept()
-print("connected to " + str(addr))
-
+#Description from Tony Nicola:
 # Alternatively you can specify the device address and I2C bus with parameters:
 #accel = Adafruit_ADXL345.ADXL345(address=0x54, busnum=2)
 
@@ -112,12 +62,20 @@ print("filename has been sent...")
 
 _time = 0.16
 
+unit = 1
+def begin():
+    input_unit = input(str("Choose the accelleration unit: \n [1]: g(=1/9.8 m/s) \n [2]: m/s(=9.8 g)\n"))
+    if input_unit == "1": unit = 1
+    elif input_unit == "2": unit = 9.8
+    else: begin()
+begin()
+
 while True:
     # Read the X, Y, Z axis acceleration values and print them.
-    x, y, z = accel.read()
-    x = x*(_time*_time)
-    y = y*(_time*_time)
-    z = z*(_time*_time)*-1
+    x, y, z = accel.read()*unit
+    x = 1/2*x*(_time*_time)
+    y = 1/2*y*(_time*_time)
+    z = 1/2*z*(_time*_time)*-1
     file = open(f"Data/{datetime}data.spmm", "w")
     data = (f"{x} {y} {z} ")
     file.write(data)
